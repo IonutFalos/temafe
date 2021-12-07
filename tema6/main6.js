9b3739dd6b6dbdbc277b61f1657a196fe721db46
Ionut Falos <ionut_falos@yahoo.it>
2021-12-07 18:25:38 +0200

    tema 6

--- tema6/main6.js
+++ tema6/main6.js
@@ -1,26 +1,8 @@
-let mypopup = 'My First Heading'
-alert(mypopup)
+let consomonautiInit = function() {
+    console.log('Salut init functie');
 
-// moving the h1 content
-var a = document.getElementById("h1").innerHTML;
-document.getElementById("h12").innerHTML = a;
-
-// moving the p1 content
-var b = document.getElementById("p11").innerHTML;
-document.getElementById("p12").innerHTML = b;
-
-// moving the p2 content
-var c = document.getElementById("p21").innerHTML;
-document.getElementById("p22").innerHTML = c;
-
-// moving the ul1 content
-var d = document.getElementById("ul1").innerHTML;
-document.getElementById("ul2").innerHTML = d;
-
-// moving the div content
-var e = document.getElementById("divParinte1").innerHTML;
-document.getElementById("divParinte2").innerHTML = e;
-
-let myarray = [1, 2, 3, 4, 5, 6, 7]
-
-console.log(myarray)
+    axios.get('https://api.spacexdata.com/v4/crew', {})
+    .then(function (response) {
+        console.log(response);
+    }); 
+};
